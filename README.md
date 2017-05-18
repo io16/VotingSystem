28e5ffc686ab6323f8c73cb31a255cab5135a3cb
io16 <igorgo12>
2017-05-18 11:09:57 +0300

    testing session

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # VotingSystem
 Курсовий проект
+request example  /savevote
+ {  "name":"test",
+    "category" : "test",
+    "questions" : {
+    	"question" :["Кількість студентів в чдту","Кількість жителів в Черкасах"]
+  	},
+    "answertoquestion":{"1" :[" 100","1000","1000","не знаю"], "2":["100 000","200 000","300 000"]
+     }
+
+
+ }
+
+ request example /getvote
+ { "idvote" : 1 }
+
+request example  /adduser
+{
+ "name" :"Alex",
+ "login":  "string" ,
+ "email":"lalla@ukr.net" ,
+ "pass":"123456"
+
+
+ }
+
--- src/golang/main.go
+++ src/golang/main.go
@@ -1,7 +1,6 @@
 package main
 //export PATH=$PATH:$GOPATH/bin
 import (
-	"net/http"
 	//"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/labstack/echo"
@@ -9,29 +8,114 @@ import (
 import (
 	"./config"
 	"./models"
+	"github.com/labstack/echo/middleware"
+
+	"net/http"
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
+	"github.com/labstack/gommon/log"
+	"github.com/gorilla/sessions"
+	"time"
 )
 
 // Create a GORM-backend model
 
 
 // Create another GORM-backend model
+var store = sessions.NewCookieStore([]byte("something-very-secret"))
 
+func Test(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
 
-func Hello(c echo.Context) error {
+		body, errJson := ioutil.ReadAll(c.Request().Body)
+		var test map[string]string
+		errJson = json.Unmarshal([]byte(body), &test)
+		if errJson != nil {
+			log.Print(errJson)
+		}
+		if test != nil {
+			log.Print(test)
+		}
 
-	return c.String(http.StatusOK, "hello")
+		cookies, errCookies := c.Request().Cookie("session-name")
+		if errCookies != nil {
+			log.Print(errCookies)
+		}
+		if errCookies != nil && errJson != nil {
+			session, err := store.Get(c.Request(), "session-name")
+			if err != nil {
+
+				return c.String(http.StatusInternalServerError, "error")
+			}
+			session.Values["foo"] = "bar"
+			session.Save(c.Request(), c.Response())
+			fmt.Println("////")
+			fmt.Println(session.Values["foo"])
+		}
+		var s string
+		if errJson == nil {
+			cookie := new(http.Cookie)
+			cookie.Name = "session-name"
+			cookie.Value = test["key"]
+			cookie.Expires = time.Now().Add(24 * time.Hour)
+			c.Request().AddCookie(cookie)
+
+			session, _ := store.Get(c.Request(), "session-name")
+			s = session.Values["foo"].(string)
+			fmt.Println(session.Values["foo"])
+
+			session.Values["foo"] = test["name"]
+			session.Save(c.Request(), c.Response())
+
+		}
+
+		fmt.Printf(cookies.Value)
+		//c.Request().AddCookie(cookies)
+
+		fmt.Println(test["key"])
+		return c.String(http.StatusOK,s )
+	}
+}
+
+func ServerHeader(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+
+		//session, err := store.Get(c.Request(), "session-name")
+		//if err != nil {
+		//
+		//	return c.String(http.StatusInternalServerError, "error")
+		//}
+		//fmt.Println(session.Values["name"])
+		//session.Values["name"] = "1"
+		return c.String(http.StatusRequestedRangeNotSatisfiable, "ok")
+	}
+
 }
 
 func main() {
 	config.InitDB()
-	config.DB.AutoMigrate(&models.VoteQuestion{}, &models.Vote{}, &models.VoteAnswerToQuestion{})
+	config.DB.AutoMigrate(&models.VoteQuestion{}, &models.Vote{}, &models.VoteAnswerToQuestion{}, models.User{}, models.Role{})
 	defer config.CloseDB()
 
 	e := echo.New()
-	e.GET("/login", Hello)
-	e.POST("/test", models.SaveVote)
-	e.POST("/getid", models.GetVote)
 
+	// Middleware
+	//e.Use(ServerHeader)
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: " time=${time_rfc3339} method=${method}, uri=${uri}, status=${status}\n",
+	}))
+	e.Use(middleware.Recover())
+	//e.Use(middleware.RecoverWithConfig(middleware.RecoverConfig{
+	//	StackSize:  1 << 10, // 1 KB
+	//}))
+	e.GET("/testsession", models.UserAuthorization, Test)
+	e.POST("/testsession", models.UserAuthorization, Test)
+	e.POST("/login", models.UserAuthorization)
+	e.POST("/savevote", models.SaveVote)
+	e.POST("/getvote", models.GetVote)
+
+	e.POST("/adduser", models.AddUser)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
